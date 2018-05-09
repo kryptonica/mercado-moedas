5a9e6e7c6b0974eddad0056eed986bbe46dc8b32
hizagi <jvitor_ferraz@hotmail.com>
2018-05-08 21:43:01 -0300

    Mudanças de interface - Anuncio

--- application/views/anuncio/cadastrar_anuncio.php
+++ application/views/anuncio/cadastrar_anuncio.php
@@ -1,38 +1,43 @@
 <section class="container" style="min-height:62.5%;">
-    <div class="row">
-        <div class="col-sm-12">
-            <h2>Cadastrar Anuncio</h2>
-            <hr>
-        </div>
-    </div>
-    <div class="row">
-    <?php $this->view('commons/alertas'); ?>
-        <form method="post" action="<?= base_url("anuncio_c/confirmar_cadastro") ?>">
-            <div class="form-group">
-                <label for="titulo_anuncio">Titulo:</label>
-                <input type="text" class="form-control" name="titulo" id="titulo_anuncio" placeholder="Titulo" value="" required>
-            </div>
-            <div class="form-group">
-                <label for="descricao_anuncio">Descrição:</label>
-                <textarea name="descricao" class="form-control" id="descricao_anuncio" rows="3" required></textarea>
-            </div>
-            <div class="form-group">
-                <label for="preco_anuncio">Preço:</label>
-                <div class="input-group">
-                <span class="input-group-addon">R$</span>
-                    <input type="number" step="0.1" class="form-control" name="preco" id="preco_anuncio" placeholder="Preço" value="" required>
-                </div>
-            </div>
+	<div class="row">
+		<div class="col-sm-12">
+			<h2>Cadastrar Anuncio</h2>
+			<hr>
+		</div>
+	</div>
+	<div class="panel panel-default">
 
-            <div class="form-group">
-                <label for="quantidade_anuncio">Quantidade:</label>
-                <input type="number" step="0.1" min="0" class="form-control" name="quantidade" id="quantidade_anuncio" placeholder="Quantidade" value="" required>
-            </div>
+		<div class="panel-body">
+			<?php $this->view('commons/alertas'); ?>
+			<form method="post" action="<?= base_url(" anuncio_c/confirmar_cadastro ") ?>">
+				<div class="form-group">
+					<label for="titulo_anuncio" class="lb">Titulo:</label>
+					<input type="text" class="form-control radius-input" name="titulo" id="titulo_anuncio" placeholder="Titulo" value="" required>
+				</div>
+				<div class="form-group">
+					<label for="descricao_anuncio" class="lb">Descrição:</label>
+					<textarea name="descricao" class="form-control radius-textarea" id="descricao_anuncio" rows="3" required></textarea>
+				</div>
+				<div class="form-group">
+					<label for="preco_anuncio" class="lb">Preço:</label>
+					<div class="input-group">
+						<span class="input-group-addon">R$</span>
+						<input type="number" step="0.1" class="form-control radius-input" name="preco" id="preco_anuncio" placeholder="Preço" value=""
+						required>
+					</div>
+				</div>
 
-            <div class="form-group">
-                <select class="custom-select select_anuncio" name="tipo_moeda" required>
-                <option value="" disabled selected>Tipo de Moeda</option>
-                <?php 
+				<div class="form-group">
+					<label for="quantidade_anuncio" class="lb">Quantidade:</label>
+					<input type="number" step="0.1" min="0" class="form-control radius-input" name="quantidade" id="quantidade_anuncio" placeholder="Quantidade"
+					value="" required>
+				</div>
+
+				<div class="form-group">
+					<label for="select_anuncio" class="lb">Tipo de moeda:</label>
+					<select class="custom-select" id="select_anuncio" name="tipo_moeda" required>
+						<option value="" disabled selected>Tipo de Moeda</option>
+						<?php 
                     foreach ($moedas as $key => $moeda) {
                         
                             echo '<option value="'.$moeda->moeda.'">'.$moeda->nome.'</option>';
@@ -41,10 +46,13 @@
                 
                 
                 ?>
-                </select>
-            </div>
-            
-            <button type="submit" class="btn btn-primary">Enviar</button><br>
-        </form>
-    </div>
+					</select>
+				</div>
+				<div class="form-group text-center">
+					<button type="submit" class="btn btn-success">
+						<span class="fa fa-check"></span> Cadastrar Anuncio </button>
+				</div>
+			</form>
+		</div>
+	</div>
 </section>
--- application/views/anuncio/editar_anuncio.php
+++ application/views/anuncio/editar_anuncio.php
@@ -1,46 +1,58 @@
 <section class="container" style="min-height:62.5%;">
-    <div class="row">
-        <div class="col-sm-12">
-            <h2>Editar Anuncio</h2>
-            <hr>
-        </div>
-    </div>
-    <script>
-        
-        <?php echo "\$id = ".$anuncio->id ?>
+	<div class="row">
+		<div class="col-sm-12">
+			<h2>Editar Anuncio</h2>
+			<hr>
+		</div>
+	</div>
+	<script>
+		<?php echo "\$id = ".$anuncio->id ?>
 
-        
+	</script>
+	<div class="panel panel-default">
 
-    </script>
-    <div class="row">
-    <?php $this->view('commons/alertas'); ?>
-        <form method="post" action="<?= base_url("anuncio_c/confirmar_atualizacao") ?>">
-        <input type="hidden" class="form-control" name="id" id="id_anuncio"  value="<?php echo $anuncio->id ?>">
-            <div class="form-group">
-                <label for="titulo_anuncio">Titulo:</label>
-                <input type="text" class="form-control" name="titulo" id="titulo_anuncio" placeholder="Titulo" value="<?php echo $anuncio->titulo ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="descricao_anuncio">Descrição:</label>
-                <textarea name="descricao" class="form-control" id="descricao_anuncio" rows="3" required><?php echo $anuncio->descricao ?></textarea>
-            </div>
-            <div class="form-group">
-                <label for="preco_anuncio">Preço:</label>
-                <div class="input-group">
-                <span class="input-group-addon">R$</span>
-                    <input type="number" step="0.1" class="form-control" name="preco" id="preco_anuncio" placeholder="Preço" value="<?php echo $anuncio->preco ?>" required>
-                </div>
-            </div>
+		<div class="panel-heading">
+			<div class="clearfix">
+				<button class="btn btn-sm btn-danger pull-right" data-toggle="modal" data-target="#modal-alterar-senha">
+					<span class="fa fa-trash"></span> Remover Anuncio</button>
+			</div>
+		</div>
 
-            <div class="form-group">
-                <label for="quantidade_anuncio">Quantidade:</label>
-                <input type="number" step="0.1" min="0" class="form-control" name="quantidade" id="quantidade_anuncio" placeholder="Quantidade" value="<?php echo $anuncio->quantidade ?>" required>
-            </div>
+		<div class="panel-body">
+			<?php $this->view('commons/alertas'); ?>
+			<form method="post" action="<?= base_url(" anuncio_c/confirmar_atualizacao ") ?>">
+				<input type="hidden" class="form-control" name="id" id="id_anuncio" value="<?php echo $anuncio->id ?>">
+				<div class="form-group">
+					<label for="titulo_anuncio" class="lb">Titulo:</label>
+					<input type="text" class="form-control radius-input" name="titulo" id="titulo_anuncio" placeholder="Titulo" value="<?php echo $anuncio->titulo ?>"
+					required>
+				</div>
+				<div class="form-group">
+					<label for="descricao_anuncio" class="lb">Descrição:</label>
+					<textarea name="descricao" class="form-control radius-textarea" id="descricao_anuncio" rows="3" required>
+						<?php echo $anuncio->descricao ?>
+					</textarea>
+				</div>
+				<div class="form-group">
+					<label for="preco_anuncio" class="lb">Preço:</label>
+					<div class="input-group">
+						<span class="input-group-addon">R$</span>
+						<input type="number" step="0.1" class="form-control radius-input" name="preco" id="preco_anuncio" placeholder="Preço" value="<?php echo $anuncio->preco ?>"
+						required>
+					</div>
+				</div>
 
-            <div class="form-group">
-                <select class="custom-select select_anuncio" name="tipo_moeda" required>
-                <option value="" disabled>Tipo de Moeda</option>
-                <?php 
+				<div class="form-group">
+					<label for="quantidade_anuncio" class="lb">Quantidade:</label>
+					<input type="number" step="0.1" min="0" class="form-control radius-input" name="quantidade" id="quantidade_anuncio" placeholder="Quantidade"
+					value="<?php echo $anuncio->quantidade ?>" required>
+				</div>
+
+				<div class="form-group">
+					<label for="select_anuncio" class="lb">Tipo de moeda:</label>
+					<select class="custom-select" id="select_anuncio" name="tipo_moeda" required>
+						<option value="" disabled>Tipo de Moeda</option>
+						<?php 
                     foreach ($moedas as $key => $moeda) {
                         
                         if( $anuncio->id_moeda == $moeda->moeda )
@@ -52,11 +64,14 @@
                 
                 
                 ?>
-                </select>
-            </div>
-            
-            <button type="submit" name="alterar" class="btn-altera btn btn-primary">Alterar</button>
-            <br>
-        </form>
-    </div>
+					</select>
+				</div>
+
+				<div class="form-group text-center">
+					<button type="submit" class="btn btn-success">
+						<span class="fa fa-check"></span>Salvar Alterações</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </section>
