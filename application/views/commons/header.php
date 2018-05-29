9cdedbbe287c1b6d85e714a8ac865609cc3c16b0
Wali Queiroz <wsantos077@gmail.com>
2018-05-28 23:51:40 -0300

    inicio notificações

--- application/views/commons/header.php
+++ application/views/commons/header.php
@@ -48,6 +48,29 @@
                                             <li><a href="<?= base_url("login_c/logout"); ?>"><span class="fa fa-sign-out"></span> Sair</a></li>
                                         </ul>
                                     </li>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="fa fa-bell"></span> (<b>2</b>)</a>
+                                        <ul class="dropdown-menu notify-drop">
+                                            <div class="notify-drop-title">
+                                                <div class="row">
+                                                    <div class="col-md-6 col-sm-6 col-xs-6">Notificações (<b>2</b>)</div>
+                                                    <div class="col-md-6 col-sm-6 col-xs-6 text-right"><a href="" class="rIcon allRead" data-tooltip="tooltip" data-placement="bottom" title="tümü okundu."><i class="fa fa-dot-circle-o"></i></a></div>
+                                                </div>
+                                            </div>
+                                            <div class="drop-content">
+                                                <li>
+                                                    <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div></div>
+                                                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                                                        <p>Lorem ipsum sit dolor amet consilium.</p>
+                                                        <p class="time">1 Saat önce</p>
+                                                    </div>
+                                                </li>
+                                            </div>
+<!--                                            <div class="notify-drop-footer text-center">
+                                                <a href=""><i class="fa fa-eye"></i> Tümünü Göster</a>
+                                            </div>-->
+                                        </ul>
+                                    </li>
                                 </ul>
                             </div>
                         <?php else: ?>
