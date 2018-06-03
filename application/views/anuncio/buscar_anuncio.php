18374fa73a791dba2f86cc1fb360e7ecb91ba0e5
Rodrigo Silva Lima <rodrig0_lima@hotmail.com>
2018-06-02 23:58:23 -0300

    - Ajustes visuais na busca

--- application/views/anuncio/buscar_anuncio.php
+++ application/views/anuncio/buscar_anuncio.php
@@ -5,7 +5,7 @@ header('Cache-Control: no cache');
 <section class="container" style="min-height:62.5%;">
 	<div class="row">
 		<div class="col-sm-12">
-			<h2>Busca</h2>
+			<h2>Busca por "<?php echo $texto; ?>"</h2>
 			<hr>
 		</div>
 	</div>
@@ -14,12 +14,12 @@ header('Cache-Control: no cache');
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="form-group">
-					<input type="text" class="form-control search" name="texto_busca" placeholder="Busca" value=<?php echo $texto; ?>>
+					<input type="text" id="campo-anuncio" class="form-control search" name="texto_busca" placeholder="Palavras-chave" value="<?php echo $texto; ?>">
 				</div>
 			</div>
-			<div class="col-sm-12">
+			<div class="col-sm-3">
 				<div class="form-group">
-					<label for="select_anuncio" class="lb">Ordenação:</label>
+					<label for="select_anuncio" class="lb">Classificar por:</label>
 					<select class="form-control" id="select_anuncio" name="ordenacao_busca">
 						<option value="maior_data_inicio" <?php echo $ordenacao=="maior_data_inicio" ? "selected": ""; ?> >Data mais antiga</option>
 						<option value="menor_data_inicio" <?php echo $ordenacao=="menor_data_inicio" ? "selected": ""; ?> >Data mais recente</option>
@@ -30,7 +30,7 @@ header('Cache-Control: no cache');
 					</select>
 				</div>
 			</div>
-			<div class="col-sm-12">
+			<div class="col-sm-9">
 					<label for="tipo_moeda" class="lb">Tipo de moeda:</label>
 				<div class="form-group">
 					<?php
@@ -39,16 +39,10 @@ header('Cache-Control: no cache');
 								$checked = in_array($moeda->moeda,$moedas_post) ? "checked" : "";
 							else
 								$checked = '';
-							echo '<label class="checkbox-inline"><input type="checkbox" name="tipo_moeda[]"  '.$checked.'  value="'.$moeda->moeda.'"> '.$moeda->nome.' </label>';
+							echo '<label class="checkbox-inline"><input type="checkbox" class="checkbox-busca" name="tipo_moeda[]"  '.$checked.'  value="'.$moeda->moeda.'"> '.$moeda->nome.' </label>';
 						}
 
 					?>
-				</div>
-			</div>
-			<div class="col-sm-1">
-				<div class="form-group text-center">
-					<button type="submit" class="btn btn-primary btn-md">
-						<span class="fa fa-filter"></span> Filtrar </button>
 				</div>
 			</div>
 		</div>
@@ -76,30 +70,29 @@ header('Cache-Control: no cache');
 
 							?>
 				<div class="col-sm-6 col-md-4">
-					<div class="thumbnail">
-						<img src="<?= base_url('assets/img/cripto_img.png'); ?>" alt="...">
-						<div class="caption">
-							<h3 >
-								<div class="max-lines">
-									<?php echo $anuncios[$i]->titulo ?>
-								</div>
-								<span class="label label-warning">
-									<?php echo $moeda_atual; ?> </span>
-							</h3>
-							<p class="anuncio_thumb_text descricao_thumb">
-								<?php echo $anuncios[$i]->descricao ?>
-							</p>
-							<p>
-								<?php echo 'Preço: R$'.$anuncios[$i]->preco ?>
-							</p>
-							<p>
-								<?php echo 'Quantidade: '.$anuncios[$i]->quantidade ?>
-							</p>
-							<p>
-								<a href="<?= base_url("visualizaranuncio?id=". $anuncios[$i]->id) ?>" class="btn btn-primary" role="button">Mais Informações</a>
-							</p>
+					<a href="<?= base_url("visualizaranuncio?id=". $anuncios[$i]->id) ?>" class="anuncio" role="button">
+						<div class="thumbnail">
+							<img src="<?= base_url('assets/img/cripto_img.png'); ?>" alt="...">
+							<div class="caption">
+								<h3 >
+									<div class="max-lines">
+										<?php echo $anuncios[$i]->titulo ?>
+									</div>
+									<span class="label label-warning">
+										<?php echo $moeda_atual; ?> </span>
+								</h3>
+								<!--p class="anuncio_thumb_text descricao_thumb">
+									<?php echo $anuncios[$i]->descricao ?>
+								</p-->
+								<h2>
+									<?php echo 'R$'.$anuncios[$i]->preco ?>
+								</h2>
+								<p>
+									<?php echo 'Quantidade: '.$anuncios[$i]->quantidade ?>
+								</p>
+							</div>
 						</div>
-					</div>
+					</a>
 				</div>
 
 				<?php $i++;
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,11 +13,24 @@ $(function () {
 
 	});
 
-        $("#quantidade-compra").change(function () {
-            $("#quantidade").val($(this).val());
-        });
-	
-	
+	$("#quantidade-compra").change(function () {
+		$("#quantidade").val($(this).val());
+	});
 
+	$('#select_anuncio').change(function(){
+		$('.form_busca').submit();
+	});
+	
+	$('.checkbox-busca').click(function(){
+		$('.form_busca').submit();
+	});
+
+	var timerid;
+	$("#campo-anuncio").keyup(function() {
+		clearTimeout(timerid);
+		timerid = setTimeout(function() {
+			$('.form_busca').submit(); 
+		}, 500);
+	});
 
 });
